9a9dcef2f01001b5f073e6e98acb52a4ad00d6f0
dannymcwaves <dannymcwaves96@gmail.com>
2018-03-19 13:45:33 +0000

    make calls to the mock fetch api and update the store with it

--- src/store/apiMiddleware.js
+++ src/store/apiMiddleware.js
@@ -1,5 +1,7 @@
 export default function () {
   return next => action => {
+    // fetch the data from the mockFetch function  and call the next function that dispatches the main action
+    // to the reducer which in turn updates the store/state.
     fetch().then(data => next({type: action.type, data: {apiResponse: data}})).catch(err => console.log(err));
   };
 }
